36dc94fd66039595bc84675abe5714bcde6c1efe
Cristinutaa <cristina.arhiliuc@insa-lyon.fr>
2018-10-23 20:55:06 +0200

    Changed everything for the inverted file.

--- venv/dataReading.py
+++ venv/dataReading.py
@@ -5,20 +5,30 @@ import string
 import time
 import re
 import nltk
+import operator
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
 
 nltk.download('stopwords')
 sw = stopwords.words('english')
+inverted_dictionary = {}
+inverted_list = {}
 
 
-def build_dictionary(array):
-    new_dictionary = {}
-    for item in array:
-        if item in new_dictionary:
-            new_dictionary[item] += 1
+def update_inverted_dictionary(array, doc_id):
+    ps = PorterStemmer()
+    for word in array:
+        item = ps.stem(word)
+        if item in sw:
+            continue
+        elif item in inverted_dictionary:
+            if doc_id in inverted_dictionary[item]:
+                inverted_dictionary[item][doc_id] += 1
+            else:
+                inverted_dictionary[item][doc_id] = 1
         else:
-            new_dictionary[item] = 1
-    return new_dictionary
+            inverted_dictionary[item] = {doc_id: 1}
 
 
 def treat_text(doc_text):
@@ -32,45 +42,53 @@ def treat_text(doc_text):
     return doc_text
 
 
-def create_doc_dictionary(doc):
+def add_doc_inverted_dictionary(doc, doc_id):
     text = ""
     for node in doc:
         for p in node.findall("P"):
             text = text + ' ' + p.text
     text = treat_text(text)
     words = text.split()
-    doc_dictionary = build_dictionary(words)
-    words_to_keep = set(doc_dictionary.keys()) - set(sw)
-    final_dictionary = {key: doc_dictionary[key] for key in words_to_keep}
-    return final_dictionary
+    update_inverted_dictionary(words, doc_id)
 
 
-def create_global_dictionary(folder):
-    articles_dictionary = {}
+def add_folder_inverted_dictionary(folder):
     for file in os.listdir(folder):
-        with open(folder + "/" + file, "r") as myfile:
-            data = "<root>" + myfile.read() + "</root>"
+        with open(folder + "/" + file, "r") as my_file:
+            data = "<root>" + my_file.read() + "</root>"
             root = ET.fromstring(data)
             for doc in root.findall("DOC"):
                 doc_id = doc.find("DOCID").text.split()[0]
-                doc_dictionary = create_doc_dictionary(doc)
-                #               doc_dictionary = {i:words.count(i) for i in set(words)}
-                articles_dictionary[doc_id] = doc_dictionary
-    return articles_dictionary
+                add_doc_inverted_dictionary(doc, doc_id)
+
+
+def create_inverted_list():
+    for key in inverted_dictionary.keys():
+        doc_dict = inverted_dictionary[key]
+        sorted_list =  sorted(doc_dict.items(), key=operator.itemgetter(1), reverse=True)
+        inverted_list[key] = sorted_list
 
 
 if __name__ == "__main__":
     data_path = input("Please write the path to the folder that contains the data \n")
     print("We are treating the data from " + data_path)
     startTime = time.time()
-    dictionary = create_global_dictionary(data_path)
+    add_folder_inverted_dictionary(data_path)
+    create_inverted_list()
     print("The treatment took %s seconds" % (time.time() - startTime))
-    export_json = input("Do you want to export the result dictionary in a json? (yes/anything else) \n")
+    export_json = input(
+            "Should we export the result dictionary with dictionaries in a json? (yes/anything else) \n")
     if export_json == "yes":
-        json = json.dumps(dictionary)
-        f = open("resources/dict.json", "w")
-        f.write(json)
+        json_dict = json.dumps(inverted_dictionary)
+        f = open("resources/dict_with_dict.json", "w")
+        f.write(json_dict)
         f.close()
-        print("Saved in resources/dict.json! Have a nice day!")
-    else:
-        print("Have a nice day!")
+        print("Saved in resources/dict_with_dict.json! Have a nice day!")
+    export_json = input(
+            "Should we export the result dictionary with lists in a json? (yes/anything else) \n")
+    if export_json == "yes":
+        json_list = json.dumps(inverted_list)
+        f = open("resources/dict_with_list.json", "w")
+        f.write(json_list)
+        f.close()
+        print("Saved in resources/dict_with_list.json! Have a nice day!")
