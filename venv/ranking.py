52df03da6346a0e4b85159e644361904e22ae728
DESKTOP-CIJ17O9\julia <julialudac@yahoo.fr>
2018-11-17 16:31:38 +0100

    ranking.py - Added Fagin's Threshold + refactored some internal functions

--- venv/ranking.py
+++ venv/ranking.py
@@ -1,6 +1,7 @@
 import json
 from nltk.stem import PorterStemmer
 import os
+import sys
 import time
 import random
 from sortedcontainers import SortedDict
@@ -59,9 +60,9 @@ def naive(query, dict_struct):
     """
     startTime = time.time()
     docs_score = {}
-    query_words = prepare_query(query)
+    query_words = __prepare_query__(query)
     # We remove the query terms that are not in dict_struct
-    query_words = remove_nonexisting_terms(query_words, dict_struct)
+    query_words = __remove_nonexisting_terms__(query_words, dict_struct)
     for qt in query_words:
         # print("final word:", qt)
         # We have to check if at least one doc contains the qt
@@ -96,10 +97,10 @@ def fagins_topk(query, K, dict_struct, dict_list):
     startTime = time.time()
     C = {}
     M = {}
-    qts = prepare_query(query) # query terms dict_struct
+    qts = __prepare_query__(query) # query terms dict_struct
 
     # We remove the query terms that are not in dict_struct
-    qts = remove_nonexisting_terms(qts, dict_struct)
+    qts = __remove_nonexisting_terms__(qts, dict_struct)
     print("kept qts:", qts)
 
     # We get the lists of pairs [docid, count of qt in doc] for the query terms
@@ -142,27 +143,25 @@ def fagins_topk(query, K, dict_struct, dict_list):
     for k,v in M.items():
         C[k] = 0
         for qt in qts: # for each dict of each qt
-            # qt is in dict_struct, according to function remove_nonexisting_terms
+            # qt is in dict_struct, according to function __remove_nonexisting_terms__
             C[k] += dict_struct[qt].get(k,0)
 
     # We want to get pairs (docscore, docid)
     # Since for now, C is composed of summed score, we will divide by #qts to get the average scores.
-    scores_list = []  # list of pairs ocscore, docid)
+    scores_list = []  # list of pairs doc scores, docid)
     nwords = len(qts)
     for k, v in C.items():
         scores_list.append((v / nwords, k))
     scores_list.sort()
-    """But we only want the ranked document id, not the scores. And until k elements"""
+    """But we only want the ranked document id, not the scores."""
     ranked_docs = []
     for pair in reversed(scores_list):  # scores must be in decreasing order
         ranked_docs.append(pair[1])
-        if len(ranked_docs) == K:
-            break
     timespent = time.time() - startTime
     return ranked_docs, timespent
 
 
-def prepare_query(query):
+def __prepare_query__(query):
     """
     :param query: the query in string to convert into a list of "conjuncted" words/terms
     :return: a set of query terms
@@ -179,7 +178,7 @@ def prepare_query(query):
     return query_words
 
 
-def remove_nonexisting_terms(qts, dict_struct):
+def __remove_nonexisting_terms__(qts, dict_struct):
     """
     Removes from qts the terms not in dict_struct
     :param qts: query terms list where we remove the non-existing terms
@@ -192,18 +191,147 @@ def remove_nonexisting_terms(qts, dict_struct):
     return newqts
 
 
+def fagins_ta(query, K, dict_struct, dict_list, epsilon=None):
+    """
+    Fagin's threshold algorithm
+    :param query: query in the form t1 OR t2 OR t3...
+    :param K: Capital K to avoid confusing with k variable used in loops
+    :param dict_struct:
+    :param dict_list:
+    :param epsilon: if None, we don't use an epsilon. Else, replace loop condition ">=tau" by ">=tau/(1+epsilon)"
+    :return:
+    Extreme cases, not specified in FA algorithm about while computing tau for one loop:
+    we exclude a qt for the computation of tau during the current loop:
+    - When there is no next doc for the qt
+    - When the next doc is the first doc (so when the qt list has not been reached yet)
+    """
+    #
+    qts = __prepare_query__(query)  # query terms
+    qts = __remove_nonexisting_terms__(qts, dict_struct)  # We remove the query terms that are not in dict_struct
+    nb_qts = len(qts)
+    finish_qts = set([])  # qts whose list is entirely browsed
+
+    # Variables' initialization: from algorithm and also intermediary variables
+    startTime = time.time()
+    C = {}  # a dict whose keys are docs d, values are mu(d). It also contains docs whose mu has been computed
+    computed_docs = []  # docs whose score has been computed. Not all are in C : those that have been removed.
+    tau = sys.maxsize
+    mu_min = sys.maxsize  # It is also the minimum score in C
+    firster_for_at_least_one_doc = []  # the list of qts whose loop for which we compute at least one mu_d
+    j = 0  # n° of while loop "Repeat until k docs in C [...]"
+
+    # The big loop
+    while not (len(C.keys()) == K and mu_min >= tau) and len(finish_qts) < nb_qts:  # Repeat until k docs in C...
+        # ... and minimum score mu is >= tau and the qts lists still have docs to compute
+        mu_min, _ = __get_mu_min_d_min__(C)
+        # print("mu_min:", mu_min)
+        # print("\n\n ----- Repeat until k docs in C and minimum score mu is >= tau -----")
+        for qt in qts:  # Sorted access in parallel to each query term
+            # print("--- Sorted access in parallel to each query term. qt:", qt, "---")
+            if j < len(dict_list[qt]):
+                d = dict_list[qt][__next_studied_term_index__(dict_list[qt], computed_docs, j)][
+                    0]  # Let d be the doc met
+                if d not in C:  # (We check of d score is not already computed)
+                    if qt not in firster_for_at_least_one_doc:
+                        firster_for_at_least_one_doc.append(qt)
+                    mud = 0  # random access to all remaining qt to compute the combined score mu(d)
+                    for qtrand in qts:
+                        mud += dict_struct[qtrand].get(d,0)
+                    mud /= nb_qts
+                    computed_docs.append(d)
+                    if len(C) < K:
+                        C[d] = mud
+                        mu_min, _ = __get_mu_min_d_min__(C)
+                    else:
+                        mu_min, d_min = __get_mu_min_d_min__(
+                            C)  # We store the doc from C with the min score as well as the min score for instruction below
+                        del C[d_min]
+                        C[d] = mud
+                        mu_min, _ = __get_mu_min_d_min__(C)
+            else:
+                finish_qts.add(qt)
+                pass
+            # print("C:", C)
+            # print("mu_min:", mu_min)
+            # print("computed_docs:", computed_docs)
+            if len(firster_for_at_least_one_doc) == nb_qts:
+                # if at_least_one_seen_everywhere:
+                tau = 0
+
+                for qt in qts:
+                    # print("for qt", qt)
+                    next_studied_qt = __next_studied_term_index__(dict_list[qt], computed_docs, j)
+                    if next_studied_qt == -1 or next_studied_qt == 0: # If there is no nex doct for the current qt or
+                                                                    # the next doc is the first doc, we ignore it
+                        finish_qts.add(qt)
+                    else:
+                        tau += dict_list[qt][next_studied_qt - 1][1]
+                tau /= nb_qts
+                # print("tau:", tau)
+        mu_min, _ = __get_mu_min_d_min__(C)
+        j += 1
+
+    # Preparing the results
+    scores_list = []  # list of pairs mu(d), d
+    for k, v in C.items():
+        scores_list.append((v, k))
+    scores_list.sort()
+    """But we only want the ranked document id, not the scores. And until K elements"""
+    ranked_docs = []
+    for pair in reversed(scores_list):  # scores must be in decreasing order
+        ranked_docs.append(pair[1])
+        if len(ranked_docs) == K:
+            break
+    timespent = time.time() - startTime
+    return ranked_docs, timespent
+
+
+def __get_mu_min_d_min__(C):
+    """
+    :param C:
+    """
+    mu_min = sys.maxsize
+    d_min = ''
+    for d in C:
+        if mu_min > C[d]:
+            mu_min = C[d]
+            d_min = d
+    return mu_min, d_min
+
+
+def __next_studied_term_index__(qt_doc_ranking, computed_docs, j):
+    """Return the minimum index/position among those of the documents that are in the list qt_doc_ranking.
+    Ex: For the list [['2', 0.9], ['5', 0.8], ['6', 0.7], ['4', 0.6], ['1', 0.5], ['3', 0.4]] and with computed_docs = ['2', '6'],
+    the returned value will be 1 (the position of ['5', 0.8])
+    """
+    next_studied_ind = j
+    try:
+        while qt_doc_ranking[next_studied_ind][0] in computed_docs:
+            next_studied_ind += 1
+        # print("+1")
+    except:  # It happens that the list has no more next doc
+        return -1
+    return next_studied_ind
+
+
 def ask_query():
     """A function to represent the process of asking queries to user"""
     query = generate_query(
         True if input("Do you want to randomly generate a query ? (yes/no)\n").lower() == "yes" else False)
     print("Your query is : " + query)
     opt = -117
-    while opt not in [0, 1]:
-        opt = int(input("Do you want to use naive or fagin's algorithm? Naive:0, Fagin's:1\n"))
+    K = -117
+    while opt not in [0, 1, 2]:
+        opt = int(input("Do you want to use naive, fagin's or fagin's threshold algorithm? "
+                        "Naive:0, Fagin's:1, Fagin's threshold:2 \n"))
     if opt == 0:
         ranked_docs, duration = naive(query, dict_struct)
+    elif opt == 1:
+        K = int(input("Please enter K:  "))
+        ranked_docs, duration = fagins_topk(query, K, dict_struct, dict_list)
     else:
-        ranked_docs, duration = fagins_topk(query, 10, dict_struct, dict_list)
+        K = int(input("Please enter K:  "))
+        ranked_docs, duration = fagins_ta(query, K, dict_struct, dict_list)
     display_result_query(ranked_docs)
     print("size of result:", len(ranked_docs))
     print("time spent:", duration)
